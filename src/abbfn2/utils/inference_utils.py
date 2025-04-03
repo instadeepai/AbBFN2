e2ff09788bea03d0054043e0ef30ff93853dce44
Miguel B <m-braganca@instadeep.com>
2025-04-03 10:52:14 +0100

    not using HF should not require token

--- src/abbfn2/utils/inference_utils.py
+++ src/abbfn2/utils/inference_utils.py
@@ -364,14 +364,16 @@ def load_params(cfg: DictConfig) -> dict[str, jax.Array]:
     
     # TODO: Once HF is open source, remove this block:
     import os
-    token = os.getenv("HF_ACCESS_TOKEN")
-    if not token:
-        raise ValueError("HF_ACCESS_TOKEN is not set!")
+   
+   
     
 >>>>>>> 0d78d23 (fixed minor logging issues)
     if cfg.load_from_hf:
+<<<<<<< HEAD
         # TODO: Once HF is open source, remove the token login:
         import os
+=======
+>>>>>>> 898ea15 (not using HF should not require token)
         token = os.getenv("HF_ACCESS_TOKEN")
         if not token:
             raise ValueError("HF_ACCESS_TOKEN is not set!")
